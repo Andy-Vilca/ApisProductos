253c9ab035557db55d1104078fecab133171aff9
Andy-Pruebas <andy.vilca@tecsup.edu.pe>
2020-07-15 16:49:41 -0500

    Apis Personas

--- models/dbconexion.js
+++ models/dbconexion.js
@@ -1,9 +1,9 @@
 var mysql = require('mysql');
 var conn = mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"",
-  database:"TodoAPI"
+  host:"x40p5pp7n9rowyv6.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user:"uhy3owhk286o1j2e",
+  password:"xw5vbohn7ilmysb9",
+  database:"dl26l7exd7c7kb2h"
 });
 conn.connect();
 module.exports = conn;
